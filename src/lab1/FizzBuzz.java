d7e05a130a0264f7be8c23a2844c0f5b8094737c
xande <xandersaiki@gmail.com>
2025-09-25 18:19:12 -0400

    Added stuff to main

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -3,7 +3,7 @@ package lab1;
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
-class FizzBuzz {
+public class FizzBuzz {
 
     public static void main(String[] args) {
 
@@ -14,7 +14,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
