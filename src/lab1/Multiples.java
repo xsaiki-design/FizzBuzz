8bbc7e3219c6f9f1ee63cc357401439a159008cc
xande <xandersaiki@gmail.com>
2025-09-25 18:33:49 -0400

    Redid the multiples to take n, a, b

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,18 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 4, 4));
+
+    }
+
+    public static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i < 1000; i++){
-            if (i % 3 == 0 || i % 5 == 0){
+        for (int i = 1; i < n; i++){
+            if (i % a == 0 || i % b == 0){
                 count++;
             }
         }
-        System.out.println(count);
+        return count;
     }
 
 }
--- test/lab1/MultiplesTest.java
+++ test/lab1/MultiplesTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class MultiplesTest {
 
-    // TODO: When you are ready to test task X,
+    // DONE: When you are ready to test task X,
     //  copy this file into the test folder.
 
     @Test
